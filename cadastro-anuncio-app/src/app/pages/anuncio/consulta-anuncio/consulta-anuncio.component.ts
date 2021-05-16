894f8db591f0e2e4572a0c70802b4b5eff043328
jutaics <jutaics@hotmail.com>
2021-05-16 18:59:52 -0300

    Realização de testes com impressão em pdf

--- cadastro-anuncio-app/src/app/pages/anuncio/consulta-anuncio/consulta-anuncio.component.ts
+++ cadastro-anuncio-app/src/app/pages/anuncio/consulta-anuncio/consulta-anuncio.component.ts
@@ -1,7 +1,9 @@
 import { AnuncioService } from 'src/app/service/anuncio/anuncio.service';
 import { Anuncio } from './../anuncio';
 import { FormGroup } from '@angular/forms';
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+
+declare var jsPDF: any;
 
 @Component({
   selector: 'app-consulta-anuncio',
@@ -25,6 +27,30 @@ export class ConsultaAnuncioComponent implements OnInit {
     this.service.list().subscribe(response => {
       this.anuncios = response;
     })
+  }
+
+  name = 'Angular';
+
+  @ViewChild('content', {static: false}) content: ElementRef;
+
+
+  public downloadPDF() {
+    const doc = new jsPDF();
+
+    const specialElementHandlers = {
+      '#editor': function (element, renderer) {
+        return true;
+      }
+    };
+
+    const content = this.content.nativeElement;
+
+    doc.fromHTML(content.innerHTML, 15, 15, {
+      width: 190,
+      'elementHandlers': specialElementHandlers
+    });
+
+    doc.save('test.pdf');
   }
 
 }
